cffc28829102c7d3b440f03238cdcf3d88c84e9e
Amine Tourki <amine.tourki@epfl.ch>
2021-04-10 12:49:23 +0200

    turn clokwise continue

--- Asspipuck/movements.c
+++ Asspipuck/movements.c
@@ -245,10 +245,16 @@ void turn_around_clockwise(void)
 			rotate_rad(M_PI/12, PAS_VIT);
 			continue;
 			}
-
-
-
-
+		if (!sensor_close_obstacle(SENSOR_1) && !sensor_close_obstacle(SENSOR_3) )
+		{
+			rotate_rad(-M_PI/12, PAS_VIT);
+			continue;
+		}
+		if (sensor_close_obstacle(SENSOR_1) && !sensor_close_obstacle(SENSOR_3) )
+		{
+			rotate_rad(M_PI/12, PAS_VIT);
+			continue;
+		}
 
 	}
 
